c7146462a888def707f65cf051ac29d6f511cd19
tdgne <not@available.yet>
2017-06-17 20:28:10 +0900

    Add some types to src/zero.rs

--- src/zero.rs
+++ src/zero.rs
@@ -18,6 +18,26 @@ impl Zero for f32 {
         0f32
     }
 }
+impl Zero for i16 {
+    fn zero() -> Self {
+        0i16
+    }
+}
+impl Zero for u16 {
+    fn zero() -> Self {
+        0u16
+    }
+}
+impl Zero for i32 {
+    fn zero() -> Self {
+        0i32
+    }
+}
+impl Zero for u32 {
+    fn zero() -> Self {
+        0u32
+    }
+}
 impl Zero for u8 {
     fn zero() -> Self {
         0u8
