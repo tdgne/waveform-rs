b0f225c82446c1a48084a9e4ab828d3b5c64d580
tdgne <not@available.yet>
2017-08-17 18:20:28 +0900

    Add multilevel binned benchmark

--- examples/benchmark.rs
+++ examples/benchmark.rs
@@ -4,6 +4,11 @@ use waveform::*;
 use std::time::SystemTime;
 
 fn main() {
+    let ratio = 1.0f64; // The ratio of samples to render.
+    let width = 1000usize; // The width of the rendered image.
+    let bin_size = 10usize; // Bin size for BinnedWaveformRenderer
+    let multi_bin_sizes: Vec<usize> = vec![10,50,100,500,1000]; // Bin sizes for MultiWaveformRenderer
+
     let mut samples: Vec<f64> = Vec::new();
     for t in 0..441000 {
         // 10 seconds
@@ -30,6 +35,25 @@ fn main() {
         },
     };
 
+    // Multilevel binned version
+    {
+        let mut wfg = MultiWaveformRenderer::new(
+            &SampleSequence {
+                data: &samples[..],
+                sample_rate: 44100f64,
+            },
+            &multi_bin_sizes,
+            config.clone(),
+        ).unwrap();
+        let now = SystemTime::now();
+        for _ in 0..100 {
+            wfg.render_vec(TimeRange::Seconds(0f64, 10f64*ratio), (width, 100));
+        }
+        if let Ok(elapsed) = now.elapsed() {
+            println!("Multi:  {} secs + {} nsecs", elapsed.as_secs(), elapsed.subsec_nanos());
+        }
+    }
+
     // Binned version
     {
         let mut wfg = BinnedWaveformRenderer::new(
@@ -37,12 +61,12 @@ fn main() {
                 data: &samples[..],
                 sample_rate: 44100f64,
             },
-            10,
+            bin_size,
             config.clone(),
         ).unwrap();
         let now = SystemTime::now();
         for _ in 0..100 {
-            wfg.render_vec(TimeRange::Seconds(0f64, 1f64), (1000, 100));
+            wfg.render_vec(TimeRange::Seconds(0f64, 10f64*ratio), (width, 100));
         }
         if let Ok(elapsed) = now.elapsed() {
             println!("Binned: {} secs + {} nsecs", elapsed.as_secs(), elapsed.subsec_nanos());
@@ -57,7 +81,7 @@ fn main() {
         };
         let now = SystemTime::now();
         for _ in 0..100 {
-            wfg.render_vec(&samples, (1000, 100));
+            wfg.render_vec(&samples[0..((441000f64*ratio) as usize)], (1000, 100));
         }
         if let Ok(elapsed) = now.elapsed() {
             println!("Direct: {} secs + {} nsecs", elapsed.as_secs(), elapsed.subsec_nanos());
