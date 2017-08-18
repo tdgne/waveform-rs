abbd0caeba26baf8466b7bda938421344e647dae
tdgne <tg.wd.29@gmail.com>
2017-08-19 06:23:13 +0900

    Update example and add comments

--- examples/waveform.rs
+++ examples/waveform.rs
@@ -1,3 +1,9 @@
+// The GUI parts conditioned by the "example-gui" feature
+// depend on the gtk crate, which is an interface to the 
+// native GTK libs.
+// Although, the other parts which are not conditioned
+// altogether demonstrate the basic use of this crate.
+
 extern crate waveform;
 
 #[cfg(feature = "example-gui")]
@@ -5,8 +11,13 @@ extern crate gtk;
 #[cfg(feature = "example-gui")]
 extern crate gdk_pixbuf;
 
-use waveform::*;
-use waveform::direct::*;
+use waveform::{
+    SampleSequence,
+    WaveformConfig,
+    Color,
+    BinnedWaveformRenderer,
+    TimeRange,
+};
 
 #[cfg(feature = "example-gui")]
 use gtk::{ContainerExt, Image, Inhibit, WidgetExt, Window, WindowExt, WindowType};
@@ -14,9 +25,6 @@ use gtk::{ContainerExt, Image, Inhibit, WidgetExt, Window, WindowExt, WindowType
 use gdk_pixbuf::Pixbuf;
 
 fn main() {
-    // Whether to use BinnedWaveformRenderer or DirectWaveformRenderer
-    let use_binned = true;
-
     #[cfg(feature = "example-gui")]
     {
         if gtk::init().is_err() {
@@ -36,6 +44,7 @@ fn main() {
         });
     }
 
+    // Generate samples to show.
     let mut samples: Vec<f64> = Vec::new();
     for t in 0..44100 {
         samples.push(
@@ -43,43 +52,55 @@ fn main() {
         );
     }
 
+    // The renderer's config.
     let config = WaveformConfig::new(
-        -1f64,
-        1f64,
+        -1f64, // Minimum amplitude to show
+        1f64, // Maximum amplitude to show
+
+        // Foreground color
         Color::RGBA {
             r: 0,
             g: 0,
             b: 0,
             a: 255,
         },
+
+        // Background color
         Color::RGBA {
             r: 0,
             g: 0,
             b: 0,
             a: 0,
-        },
+        }
     ).unwrap();
 
-    let vec: Vec<u8>;
-    if use_binned {
-        let ss = SampleSequence {
-            data: &samples[..],
-            sample_rate: 44100f64,
-        };
-        let wfg = BinnedWaveformRenderer::new(&ss, 10, config).unwrap();
-        vec = wfg.render_vec(TimeRange::Seconds(0.0f64, 1.0f64), (800, 100))
-            .unwrap();
-    } else {
-        let wfg = DirectWaveformRenderer {
-            sample_rate: 44100f64,
-            config: config,
-        };
-        vec = wfg.render_vec(&samples, (800, 100)).unwrap();
-    }
+    // Put a reference to the samples here along with its sample rate.
+    // We need to set a sample rate because it will be used
+    // when you specify the time range in seconds.
+    let ss = SampleSequence {
+        data: &samples[..],
+        sample_rate: 44100f64,
+    };
+
+    // Construct the renderer.
+    // The second argument is the bin size.
+    // The renderer will bin the samples, and then
+    // calculate the minimum and maximum amplitude values for
+    // each bin.
+    let wfg = BinnedWaveformRenderer::new(&ss, 10, config).unwrap();
+
+    // Render!
+    // The renderer doesn't look at the actual audio samples here.
+    // Instead it will use the binned min/max values calculated above,
+    // making the rendering quite faster.
+    let vec: Vec<u8> =
+        wfg.render_vec(TimeRange::Seconds(0.0f64, 1.0f64), (800, 100))
+        .unwrap();
 
     #[cfg(feature = "example-gui")]
     {
-        let pixbuf = Pixbuf::new_from_vec(vec, 0, true, 8, 800, 100, 800 * 4);
+        let pixbuf =
+            Pixbuf::new_from_vec(vec, 0, true, 8, 800, 100, 800 * 4);
         let image = Image::new_from_pixbuf(Some(&pixbuf));
         window.add(&image);
         window.show_all();
